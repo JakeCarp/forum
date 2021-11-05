560d120c87a73649fa945d79dfb9c1a9137276f8
jacob carpenter <jakecarpenter407@gmail.com>
2021-11-05 10:20:08 -0600

    get all route done

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -1,8 +1,35 @@
 import { postsService } from '../services/PostsService'
-import { logger } from '../utils/Logger'
+import BaseController from '../utils/BaseController'
+import { Auth0Provider } from '@bcwdev/auth0provider'
 
-export class PostsController {
+export class PostsController extends BaseController {
   constructor() {
-    logger.log('hello from posts controller', postsService)
+    super('api/posts')
+    this.router
+      .get('', this.getAll)
+    //   .get(':/posts/king', this.getKing)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.create)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const query = req.query
+      const posts = await postsService.getAll(query)
+      return res.send(posts)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const postData = req.body
+      postData.creatorId = req.userInfo.id
+      const post = await postsService.create(postData)
+      return res.send(post)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,4 +1,14 @@
-class PostsService {
+import { dbContext } from '../db/DbContext'
 
+class PostsService {
+  async getAll(query = {}) {
+    const posts = dbContext.Posts.find(query)
+    return posts
+  }
+
+  async create(postData) {
+    const post = await dbContext.Posts.create(postData)
+    return post
+  }
 }
 export const postsService = new PostsService()
